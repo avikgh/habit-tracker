b10d29671c77fbeb02b39dd30a5069f5321a4766
12799 <avikgh21@gmail.com>
2025-10-23 03:17:12 +0600

    Validate the sign in form

--- lib/ui/screens/auth/sign_in_screen.dart
+++ lib/ui/screens/auth/sign_in_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/ui/screens/choose_habit_screen.dart';
 import 'package:habit_tracker/ui/widgets/custom_elevated_button.dart';
+import 'package:habit_tracker/utilities/app_constraints.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -28,6 +30,16 @@ class _SignInScreenState extends State<SignInScreen> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: TextFormField(
+                validator: (value) {
+                  if (value?.trim().isEmpty ?? true) {
+                    return 'Enter an email address';
+                  }
+                  if (AppConstrains.emailRegExp.hasMatch(value!) == false) {
+                    return 'Enter a valid email address';
+                  }
+                  return null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
                 decoration: InputDecoration(hintText: 'Email'),
               ),
             ),
@@ -36,6 +48,13 @@ class _SignInScreenState extends State<SignInScreen> {
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: TextFormField(
                 obscureText: _isPasswordVisible,
+                validator: (value) {
+                  if (value?.trim().isEmpty ?? true) {
+                    return 'Enter a valid password';
+                  }
+                  return null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
                 decoration: InputDecoration(
                     hintText: 'Password',
                     suffixIcon: GestureDetector(
@@ -53,7 +72,12 @@ class _SignInScreenState extends State<SignInScreen> {
             Padding(
               padding: const EdgeInsets.all(25.0),
               child: CustomElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ChooseHabitScreen()));
+                },
                 buttonName: 'Sign In',
               ),
             ),
